2343403156b7b9a1633fcf3f24d62d99ff94039e
jaejun <zoonie04@khu.ac.kr>
2024-11-19 11:51:17 +0900

    add account delete

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 # 
 
 urlpatterns = [
+    path('delete/', views.user_delete), # 회원탈퇴
 ]
--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import *
+from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 # Create your views here.
+
+#############구분선#####################
+# 회원탈퇴
+@api_view(['POST'])
+@permission_classes([IsAuthenticated]) # 인증된 사용자만 권한 허용
+def user_delete(request):
+    request.user.delete()
+    data = {
+            'content': f'{request.user}님의 탈퇴처리가 완료되었습니다.',
+        }
+    return Response(data, status=status.HTTP_204_NO_CONTENT)
+
