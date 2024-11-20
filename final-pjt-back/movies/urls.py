ed8a448c21992e115650286929376f32ac2c55d3
jaejun <zoonie04@khu.ac.kr>
2024-11-20 23:35:23 +0900

    make recommedation algorithm / feed collecting by user data

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import FeedCreateView, MovieCreateView
+from .views import *
 
 app_name = 'movies'
 
 urlpatterns = [
     path('feed/create/', FeedCreateView.as_view(), name='feed_create'),
     path('movie/create/', MovieCreateView.as_view(), name='movie_create'),
+    path('recommend/', recommend_movies_with_rating, name='recommend_movies_with_rating'),
+    path('feeds/<int:user_id>/', user_feed_list, name='user_feed_list'),
 ]
