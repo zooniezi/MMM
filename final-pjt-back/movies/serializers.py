d0382a3f053b0575ee860fe7986476e3efd61dfb
jaejun <zoonie04@khu.ac.kr>
2024-11-20 16:02:38 +0900

    add moviecreate

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Feed
+from .models import Feed, Movie
 
 class FeedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feed
         fields = '__all__'  # 모든 필드 포함
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'  # 모든 필드를 직렬화
