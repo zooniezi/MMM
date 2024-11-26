03ad7089c677e54412d2fd7c1873f3e4048f7090
jaejun <zoonie04@khu.ac.kr>
2024-11-26 15:54:28 +0900

    add profile in user model

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    profile = models.TextField(blank=True)
     # profile_image = models.ImageField(upload_to='profile_images/', blank=True)
     def __str__(self):
         return self.username
