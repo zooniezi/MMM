2fb638b3858594dba9145a6d273b5497609753f8
jaejun <zoonie04@khu.ac.kr>
2024-11-26 17:07:41 +0900

    fix usermodel

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    profile = models.TextField(blank=True)
+    profile = models.TextField(blank=True, null=True)
     # profile_image = models.ImageField(upload_to='profile_images/', blank=True)
     def __str__(self):
         return self.username
