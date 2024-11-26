2b5cdd9eb1ca7da49d5c498eac8f1b450af92f2a
jaejun <zoonie04@khu.ac.kr>
2024-11-27 00:44:44 +0900

    add achievements

--- final-pjt-back/achievements/views.py
+++ final-pjt-back/achievements/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from .models import Achievement
+from django.http import JsonResponse
 # Create your views here.
-def fresh_start(request):
-    pass
+def get_user_achievement(request, user_id):
+    user = get_object_or_404(get_user_model(), id=user_id)
+
+    achievements = Achievement.objects.filter(user=user)
+    data = []
+
+    for achievement in achievements:
+        data.append({
+            "achievement_id": achievement.achievement_id
+        })
+    
+    return JsonResponse(data, safe=False)
